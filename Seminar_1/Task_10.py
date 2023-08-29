95c1c9a58e1a6dc92ea97265793dc08e345ed0a3
Grandhald <kel_tua@mail.ru>
2023-08-29 19:17:46 +0300

    appdate

---
+++ Seminar_1/Task_10.py
@@ -0,0 +1,10 @@
+ls = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
+{"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, 
+{" VIII":" S007 "}] 
+
+uni_elem = set([value.replace(" ", "") for item in ls for value in item.values() ])
+
+
+    
+       
+print(uni_elem)
